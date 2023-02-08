d204df857f1280f1af43b035571037f08d9676c1
LuisRangel <lr2315490@gmail.com>
2023-02-08 10:25:15 -0600

    Back

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,16 @@ const router = require('express').Router()
 const User = require('../models/User')
 const Joi = require('@hapi/joi')
 const bcrypt = require('bcrypt')
+const Jwt = require('jsonwebtoken')
 
 const schemaRegister = Joi.object({
     name: Joi.string().min(6).max(255).required(),
     lastname: Joi.string().max(255).required(),
+    email: Joi.string().max(1024).required(),
+    password: Joi.string().min(6).required()
+})
+
+const schemaLogin = Joi.object({
     email: Joi.string().max(1024).required(),
     password: Joi.string().min(6).required()
 })
@@ -46,6 +52,162 @@ router.post('/registre', async(req, res) => {
         res.status(400).json({
             message: 'Error al guardar',
             error
+        })
+    }
+})
+
+router.post('/actualizar', async(req, res) => {
+    //Actualizar de usuario
+    const { error } = schemaRegister.validate(req.body) 
+    if(error){
+       return res.status(400).json({
+            error: error.details[0].message
+        })
+    }
+
+    const isEmailUnique = await User.findOne({email: req.body.email})
+    if (isEmailUnique) {
+        return res.status(400).json({
+            error: "El correo ya existe"
+        })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const passwordEncriptado = await bcrypt.hash(req.body.password, salt)
+
+    const usuario = new User({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: passwordEncriptado, 
+    })
+
+    try {
+       const usuario = await usuario.findBypk(id)
+       if(!usuario){
+        return res.status(404).json({
+            message: "No existe el usuario con el id: " + id
+        });
+       }
+       await usuario.update(body);
+       
+    } catch (error) {
+        res.status(400).json({
+            message: 'Error al guardar',
+            error
+        })
+    }
+})
+
+router.post('/actualizarr', async(req, res) => {
+    //Validacion de usuario
+    const { error } = schemaRegister.validate(req.body) 
+    if(error){
+       return res.status(400).json({
+            error: error.details[0].message
+        })
+    }
+
+    const isEmailUnique = await User.findOne({email: req.body.email})
+    if (isEmailUnique) {
+        return res.status(400).json({
+            error: "El correo ya existe"
+        })
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const passwordEncriptado = await bcrypt.hash(req.body.password, salt)
+
+    const usuario = new User({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: passwordEncriptado, 
+    })
+
+    try {
+        const guardado = await usuario.findAndModify({
+         id: usuario.id
+        }, [['id', 'asc']],{
+         $set:{
+             "username":usuario.name,
+             "email":usuario.email
+         }
+        })
+        res.json({
+         message: 'Success',
+         data: guardado
+        })
+     } catch (error) {
+         res.status(400).json({
+             message: 'Error al guardar',
+             error
+         })
+     }
+})
+
+router.post('/login', async(req, res)=> {
+    //login de usuario
+    const { error } = schemaLogin.validate(req.body) 
+    if(error){
+       return res.status(400).json({
+            error: error.details[0].message
+        })
+    }
+
+    const isEmailUnique = await User.findOne({email: req.body.email})
+    if (!isEmailUnique) {
+        return res.status(400).json({
+            error: "El correo no existe"
+        })
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, isEmailUnique.password)
+    if (!validPassword) {
+        return res.status(400).json({
+            error: "Password Incorrecto"
+        })
+    }
+
+    const token = Jwt.sign({
+        name: isEmailUnique.name,
+        id: isEmailUnique.id
+    }, process.env.TOKEN_SECRET)
+
+    res.header('auth-token', token).json({
+        error: null,
+        data: { token }
+    })
+})
+
+router.get('/getallusers', async(req, res) => {
+    const users = await User.find()
+
+    if (users){
+        res.json({
+            error: null,
+            data: users
+        })
+    }else{
+        return res.status(400).json({
+            error: "No hay usuarios"
+        })
+    }
+})
+
+router.post('/erauser', async(req, res) => {
+    const id = req.body.id
+
+    const erased = await User.findByIdAndDelete(id)
+
+    if (erased) {
+        res.json({
+            error: null,
+            message: "Borrado Satisfactoriamente"
+        })
+    }else{
+        return res.status(400).json({
+            error: "No se pudo borrar el usuario"
         })
     }
 })
